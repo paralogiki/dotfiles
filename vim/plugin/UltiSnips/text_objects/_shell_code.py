c69f3867bd1daeb0f607d56acae0dafa53a3fe28
Keith Welch <keith@paralogiktech.com>
2013-04-06 22:28:31 -0400

    Reverted _shell_code.py back

--- vim/plugin/UltiSnips/text_objects/_shell_code.py
+++ vim/plugin/UltiSnips/text_objects/_shell_code.py
@@ -17,42 +17,26 @@ class ShellCode(NoneditableTextObject):
 
     def _update(self, done, not_done):
         # Write the code to a temporary file
-        userdir = os.path.expanduser("~")
-        output = ''
-        stderr = ''
-        for tmpdir in [tempfile.gettempdir(),userdir+'/.cache',userdir+'/.tmp',userdir]:
-          if os.path.exists(tmpdir) == 0:
-            continue;
-          handle, path = tempfile.mkstemp(text=True,dir=tmpdir)
-          os.write(handle, self._code.encode("utf-8"))
-          os.close(handle)
-          os.chmod(path, stat.S_IRWXU)
+        handle, path = tempfile.mkstemp(text=True)
+        os.write(handle, self._code.encode("utf-8"))
+        os.close(handle)
+        os.chmod(path, stat.S_IRWXU)
 
-          # Execute the file and read stdout
-          proc = subprocess.Popen(path, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-          proc.wait()
-          stdout, stderr = proc.communicate()
-          if len(stdout) == 0 and len(stderr) > 0:
-            continue
-          output = as_unicode(stdout)
+        # Execute the file and read stdout
+        proc = subprocess.Popen(path, shell=True, stdout=subprocess.PIPE)
+        proc.wait()
+        output = as_unicode(proc.stdout.read())
 
-          if len(output) and output[-1] == '\n':
-              output = output[:-1]
-          if len(output) and output[-1] == '\r':
-              output = output[:-1]
+        if len(output) and output[-1] == '\n':
+            output = output[:-1]
+        if len(output) and output[-1] == '\r':
+            output = output[:-1]
 
-          os.unlink(path)
+        os.unlink(path)
 
-        if len(output) == 0:
-		output = "Error '" + self._code.encode('utf-8') + "' returned empty result" 
-          if len(stderr) > 0:
-            if len(stderr) and stderr[-1] == '\n':
-                stderr = stderr[:-1]
-            if len(stderr) and stderr[-1] == '\r':
-                stderr = stderr[:-1]
-            output += ", stderr: " + stderr
         self.overwrite(output)
         self._parent._del_child(self)
 
         return True
+
 
